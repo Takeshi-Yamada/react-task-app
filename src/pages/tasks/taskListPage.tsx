5f1fb7bf4424ca7606c116c424c1673a98695852
takeshi <takeshi.yamada1010@gmail.com>
2025-09-24 12:56:41 +0900

    first commit

---
+++ src/pages/tasks/taskListPage.tsx
@@ -0,0 +1,12 @@
+import TaskListContainer from "../../features/tasks/containers/TaskListContainer";
+
+export default function TaskListPage() {
+  return (
+    <div>
+      <TaskListContainer />
+        {
+          
+        }
+    </div>
+  )
+}
